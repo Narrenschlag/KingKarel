af36d914cae984b6cc0551547b89aefc0e7a1c20
Maximilian Schecklmann <narrenschlag@gmail.com>
2022-10-18 14:50:42 +0200

    0.0.7
    No/Recursive solution sample for doubleBeepers

--- KingKarel/Samples/doubleBeepers.cs
+++ KingKarel/Samples/doubleBeepers.cs
@@ -2,35 +2,104 @@
 {
     internal class doubleBeepers : Backend
     {
+        // Runs when application is started
         public void setup()
         {
             Levels.Load(Levels.doubleBeepers);
         }
 
-        void recursive()
+        #region Recursion Sample
+        void recursion()
         {
+            // Pick up beeper
             pickBeeper();
 
-            if (beepersPresent()) recursive();
+            // Recurse until no beepers left
+            if (beepersPresent()) recursion();
 
-            // Place two new beepers
-            for (int i = 0; i < 2; i++)
-                putBeeper();
+            // Place two new beepers for every picked beeper
+            for (int i = 0; i < 2; i++) putBeeper();
         }
+        #endregion
 
+        #region No Recursion Sample
+        void noRecursion()
+        {
+            // While beepers are at origin
+            while (beepersPresent())
+            {
+                // Pick up beeper
+                pickBeeper();
+
+                // Move to the right
+                move();
+
+                // Place two new beepers at point to the right
+                for (int i = 0; i < 2; i++) putBeeper();
+
+                // Turn around
+                turnAround();
+
+                // Move to origin
+                move();
+
+                // Get into original position by turning around again
+                turnAround();
+            }
+
+            // Move to the storage point
+            move();
+
+            // Turn around to get into new origin position
+            turnAround();
+
+            // While beepers are at storage
+            while (beepersPresent())
+            {
+                // Pick up beeper
+                pickBeeper();
+
+                // Move to the left
+                move();
+
+                // Put beeper
+                putBeeper();
+
+                // Turn around
+                turnAround();
+
+                // Move to the right
+                move();
+
+                // Turn around to get back into original position
+                turnAround();
+            }
+
+        }
+        #endregion
+
+        // Turn by 180°
         void turnAround()
         {
             for (int i = 0; i < 2; i++) turnLeft();
         }
 
+        // Runs after setup
         public void run()
         {
+            // Move to the beepers
             while (noBeepersPresent()) move();
 
-            recursive();
-            turnAround();
+            // Solution of your choice
+            noRecursion();
 
-            move();
+            // Turn until facing west
+            while (!facingWest()) turnLeft();
+
+            // Move until wall is reached
+            while (frontIsClear()) move();
+
+            // Turn around to face east
             turnAround();
         }
     }
